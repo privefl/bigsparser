3677f3bcbfd8c49fdd0145f9552e1259021f51ea
Florian Privé <florian.prive.21@gmail.com>
2020-04-07 15:30:41 +0200

    back in inst/

--- inst/include/bigsparser/SFBM.h
+++ inst/include/bigsparser/SFBM.h
@@ -47,13 +47,47 @@ public:
   }
 
   template<class C>
-  C prod(const C& x);
+  C prod(const C& x) {
+
+    C res(n);
+    for (int i = 0; i < n; i++) res[i] = 0;
+
+    for (int j = 0; j < m; j++) {
+
+      auto lo = data + p[j];
+      auto up = data + p[j + 1];
+
+      for (auto it = lo; it != up; it++) {
+        res[it->i] += it->x * x[j];
+      }
+    }
+
+    return res;
+  }
 
   template<class C>
-  C cprod(const C& x);
+  C cprod(const C& x) {
+
+    C res(m);
+
+    for (int j = 0; j < m; j++)
+      res[j] = dot_col(j, x);
+
+    return res;
+  }
 
   template<class C>
-  double dot_col(int j, const C& x);
+  double dot_col(int j, const C& x) {
+
+    auto lo = data + p[j];
+    auto up = data + p[j + 1];
+
+    double cp = 0;
+    for (auto it = lo; it != up; it++)
+      cp += it->x * x[it->i];
+
+    return cp;
+  }
 
 private:
   mio::mmap_source ro_mmap;
